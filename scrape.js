64fe34b7aa1deacbc3624d6142830d5795be96e6
luke-ols0 <luke.olson@outlook.com>
2020-01-29 02:59:40 -0600

    scrape.js remade with promises

--- scrape.js
+++ scrape.js
@@ -1,43 +1,89 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-
 const fs = require('fs');
-const writeStream = fs.createWriteStream('post.csv');
-
-// Write Headers
-// writeStream.write(`Title,Link,Date \n`);
 
 
-// gb 4gHs8pWsgZpndQZKs6QVRH
-// dd 5Z3IWpvwOvoaWodujHw7xh
-var arstistId = '5Z3IWpvwOvoaWodujHw7xh'
-request('https://open.spotify.com/artist/' + arstistId + '/about', (error, response, html) => {
+var arstistID = '3yY2gUcIsjMr8hjo51PoJ8'
+request('https://open.spotify.com/artist/' + arstistID + '/about', (error, response, html) => {
     if (!error && response.statusCode == 200) {
+
+        // Loads html page into cheerio
         const $ = cheerio.load(html);
 
-        eval($('script')[7].children[0].data)
+        // creates accessable variables
+        let rawSpotifyData;
+        let artistGenres = [];
+        let cityData = [];
+        let relatedArtistIDs = [];
 
-        // 5kyTqxwLNQk50dJZIzFQuq
-        // Retrieve
-        console.log('\nArtist Genres:')
-        console.log(Spotify.Entity.genres);
-
-        console.log('\nArtist Cities With Listener Data:')
-        console.log(Spotify.Entity.insights.cities);
-
-        // Creates list of related artists
-        const relatedArtists = $('.cover.artist');
-        var relatedArtistIds = [];
-        $('.cover.artist').each(function () {
-            var link = $(this).attr('href');
-            relatedArtistIds.push({ "link": link });
+        // Function to convert scrapeD html <script> tag string into json object
+        const pullObject = new Promise(function (resolve, reject) {
+            // Loads Spotify data from <script> tag using cheerio
+            rawSpotifyData = $('script')[7].children[0].data;
+            resolve(rawSpotifyData);
         });
 
-        // logs related artist ids to console.
-        console.log('\nRelated artist Ids:');
-        console.log(relatedArtistIds);
+        // Loads rawSpotifyData with Cheerio
+        pullObject
+            .then
+            // String slice function using rawSpotifyData
+            (function (rawSpotifyData) {
 
-        console.log('\nScraping Done...');
+                // Slices string to convert into object containing Spotify data
+                var spotifyDataString = rawSpotifyData.slice(48, -6);
+
+                // Returns spotifyDataString
+                return (spotifyDataString);
+            })
+            .then
+            // Parses Spotify data into object function
+            (function (spotifyDataString) {
+
+                // Parses Spotify data string into object
+                spotifyObject = JSON.parse(spotifyDataString);
+                // Returns spotify object
+                return (spotifyObject);
+            })
+            .then
+            // Function to display object Spotify genre, city, and related artist data in console
+            (function (spotifyObject) {
+
+                console.log('\n~~~~~~~~~~~~~~~~~~~~ Spotify Web Scraping Using Cheerio ~~~~~~~~~~~~~~~~~~~~');
+
+                // Assigns spotifyObject.genres to 'artistGenres' object
+                artistGenres = spotifyObject.genres;
+
+                // Displays Spotify genres of specified artist
+                console.log('\nArtist Genres:');
+                console.log(artistGenres);
+
+                // Assigns spotifyObject.insights.cities to 'cityData' object
+                cityData = spotifyObject.insights.cities;
+
+                // Displays Spotify city listener data of specified artist
+                console.log('\nArtist Cities With Listener Data:')
+                console.log(cityData);
+
+                // Scrapes list of Spotify related artists with cheerio
+                const relatedArtists = $('.cover.artist');
+                $('.cover.artist').each(function () {
+                    var link = $(this).attr('href');
+                    // Slices to push only Spotify artist ID
+                    relatedArtistIDs.push({ "id": link.slice(8) });
+                });
+
+                // Displays Spotify related artist IDs to console.
+                console.log('\nRelated artist Ids:');
+                console.log(relatedArtistIDs);
+
+                console.log('\n ~~~~~~~~~~~~~~~~~~~~ Scraping Complete ~~~~~~~~~~~~~~~~~~~~');
+            },
+                function (error) {
+                    // Common error handling
+                    console.log('There was an error:\n\n' + error);
+                }
+            );
     }
 });
+
